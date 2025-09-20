8c546ca0d5b269eaef4a6691de959b4eed12fc43
vladimir-molotkov <molotkov1vl@gmail.com>
2025-09-20 11:01:51 +0300

    fix

--- main.py
+++ main.py
@@ -3,30 +3,32 @@ from scripts.speech_to_text import AudioTranscriber
 from scripts.text_summarization import TextSummarizer
 
 
-def main():
-    file_name = "lec1"
+def main(file_name=None):
+    # file_name = "Занятие 2"
     source_dir = "/Users/vladimir/Desktop/Материалы курса по античности"
     temp_dir = "/Users/vladimir/Desktop/Материалы курса по античности/temp"
-    target_dir = "/Users/vladimir/Obsidian/Ancient_History"
+    target_dir = "/Users/vladimir/Obsidian/Roman_law"
 
-    mp4_file_path = f"{source_dir}/{file_name}.mp4"
-    mp3_file_path = f"{temp_dir}/{file_name}.mp3"
+    video_file_path = f"{source_dir}/{file_name}.mp4"
+    audio_file_path = f"{temp_dir}/{file_name}.mp3"
     transcription_file_path = f"{temp_dir}/{file_name}.txt"
     summary_file_path = f"{target_dir}/{file_name}.md"
 
     # Step 1: Convert MP4 → MP3
-    AudioExtractor.mp4_to_mp3(mp4_file_path, mp3_file_path)
+    AudioExtractor.mp4_to_mp3(video_file_path, audio_file_path)
 
     # Step 2: Transcribe MP3 → TXT
     transcriber = AudioTranscriber(model_size="turbo", language="ru")
-    transcriber.transcribe(mp3_file_path, transcription_file_path)
+    transcriber.transcribe(audio_file_path, transcription_file_path)
 
     # Step 3: Summarize TXT → Markdown
     summarizer = TextSummarizer()
     summarizer.process_file(transcription_file_path, summary_file_path)
 
-    print(f"\n All steps completed successfully for file {mp4_file_path}")
+    print(f"\n All steps completed successfully for file {video_file_path}")
 
 
 if __name__ == "__main__":
-    main()
+    lectures_to_convert = [f"Лекция {i}" for i in range(1, 19)] + ["Лекция 20"]
+    for file in lectures_to_convert:
+        main(file_name=file)
